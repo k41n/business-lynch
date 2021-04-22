ada2fb820233bab8d0255b88fc6725c8889e046b
Andrei Malyshev <k41n@shuttlerock.com>
2021-04-22 16:14:16 +0300

    Change cache to redis since Vercel does not support node-cache

--- helpers/cache.ts
+++ helpers/cache.ts
@@ -1,3 +1,20 @@
-import NodeCache from 'node-cache';
+import redis from 'redis';
 
-export const cache = new NodeCache({ stdTTL: 60 * 60 * 1 });
+var client = redis.createClient({
+  host: process.env.NEXT_REDIS_HOST,
+  port: process.env.NEXT_REDIS_PORT,
+  password: process.env.NEXT_REDIS_PASSWORD,
+});
+
+client.on('error', function (err) {
+  throw err;
+});
+
+export const cache = {
+  set: (key: string, value: any) => 
+    client.set(key, JSON.stringify(value)),
+  get: (key: string) =>
+    client.get(key),
+  delete: (key: string) =>
+    client.delete(key),
+}
